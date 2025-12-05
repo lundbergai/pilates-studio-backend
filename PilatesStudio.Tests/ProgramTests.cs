fffba714339c67ed413e18c2b0cc9ee76ba64e64
Viktor Lundberg <vikkolundberg@gmail.com>
2025-12-05 15:48:01 +0100

    add test workflow with one test

---
+++ PilatesStudio.Tests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿namespace PilatesStudio.Tests;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Program_ClassExists()
+    {
+        // Arrange & Act
+        var programType = typeof(Program);
+
+        // Assert
+        Assert.NotNull(programType);
+        Assert.Equal("Program", programType.Name);
+    }
+}
